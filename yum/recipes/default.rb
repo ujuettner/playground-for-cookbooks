440f7d325f697a3583623775ac532d72c7600fc8
Udo Juettner <udo.juettner@gmail.com>
2013-04-08 18:43:13 +0200

    Use flush_cache before package installation.

--- yum/recipes/default.rb
+++ yum/recipes/default.rb
@@ -7,11 +7,11 @@ remote_file '/etc/yum.repos.d/boundary.repo' do
   source 'https://yum.boundary.com/boundary_centos6_64bit.repo'
   mode '0644'
   action :create_if_missing
-  notifies :run, resources(:execute => 'clean-yum-cache')
+  notifies :run, resources(:execute => 'clean-yum-cache'), :immediately
 end
 
 yum_package 'bprobe' do
     action :install
-    #flush_cache [:before]
+    flush_cache [:before]
 end
 
