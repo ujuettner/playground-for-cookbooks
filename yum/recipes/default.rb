982534e7de78c9c07e8b3e2e2c4bcfaf5342f2b0
Udo Juettner <udo.juettner@gmail.com>
2013-04-10 19:15:28 +0200

    Fix a typo.

--- yum/recipes/default.rb
+++ yum/recipes/default.rb
@@ -1,5 +1,5 @@
 execute 'clean-yum-cache' do
-  command 'rm -f /var/lib/rpm/__db* && rpm –rebuilddb && yum clean all'
+  command 'rm -f /var/lib/rpm/__db* && rpm --rebuilddb && yum clean all'
   action :run
 end
 
