d5e8c2b5378f769d3a0126a0e866408c09d4a1e3
Udo Juettner <udo.juettner@gmail.com>
2013-04-11 14:25:16 +0200

    Next try.

--- yum/recipes/default.rb
+++ yum/recipes/default.rb
@@ -1,5 +1,5 @@
 execute 'clean-yum-cache' do
-  command 'rm -f /var/lib/rpm/__db* && rpm --rebuilddb && yum clean all && yum makecache && yum check && repoquery --disablerepo=* --enablerepo=boundary -a'
+  command 'rm -f /var/lib/rpm/__db* && rpm --rebuilddb && yum clean all && rm -rf /var/cache/yum && yum makecache && yum check && repoquery --disablerepo=* --enablerepo=boundary -a'
   action :nothing
 end
 
