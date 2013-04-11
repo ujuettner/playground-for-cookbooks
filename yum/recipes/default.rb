3f0c8c5984ec83b19930875df6612882f9fc32d1
Udo Juettner <udo.juettner@gmail.com>
2013-04-11 17:31:17 +0200

    Next try.

--- yum/recipes/default.rb
+++ yum/recipes/default.rb
@@ -6,7 +6,7 @@ end
 yum_repo.run_action(:create_if_missing)
 
 clean_yum_cache = execute 'clean-yum-cache' do
-  command 'echo "== REMOVE RPM DB ===" && rm -f /var/lib/rpm/__db* && echo "=== REBUILD RPM DB ===" && rpm --rebuilddb && echo "=== YUM CLEAN ALL ===" && yum clean all && echo "=== REMOVE YUM CACHE ===" && rm -rf /var/cache/yum && echo "=== CREATE YUM CACHE ===" && yum makecache && echo "=== CHECK YUM ===" && yum check && echo "=== ENABLE REPO ===" && yum-config-manager --quiet --enable boundary && echo "=== QUERY NEW REPO ===" && repoquery --disablerepo=* --enablerepo=boundary -a && echo "=== YUM PKG INFO ===" && yum info bprobe'
+  command 'echo "== REMOVE RPM DB ===" && rm -f /var/lib/rpm/__db* && echo "=== YUM CLEAN ALL ===" && yum clean all && echo "=== REMOVE YUM CACHE ===" && rm -rf /var/cache/yum && echo "=== ENABLE REPO ===" && yum-config-manager --quiet --enable boundary && echo "=== CREATE YUM CACHE ===" && yum makecache && echo "=== CHECK YUM ===" && yum check && echo "=== REBUILD RPM DB ===" && rpm --rebuilddb && echo "=== QUERY NEW REPO ===" && repoquery --disablerepo=* --enablerepo=boundary -a && echo "=== YUM PKG INFO ===" && yum info bprobe'
   action :nothing
 end
 clean_yum_cache.run_action(:run)
