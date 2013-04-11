53210703ccd4deeae8495270bc7fad8b611cb395
Udo Juettner <udo.juettner@gmail.com>
2013-04-11 18:28:49 +0200

    Next try.

--- yum/recipes/add_repo.rb
+++ yum/recipes/add_repo.rb
@@ -1,6 +1,5 @@
-
-execute 'clean-yum-cache' do
-  command 'echo "== REMOVE RPM DB ===" && rm -f /var/lib/rpm/__db* && echo "=== YUM CLEAN ALL ===" && yum clean all && echo "=== REMOVE YUM CACHE ===" && rm -rf /var/cache/yum && echo "=== ENABLE REPO ===" && yum-config-manager --quiet --enable boundary && echo "=== CREATE YUM CACHE ===" && yum makecache && echo "=== CHECK YUM ===" && yum check && echo "=== REBUILD RPM DB ===" && rpm --rebuilddb && echo "=== QUERY NEW REPO ===" && repoquery --disablerepo=* --enablerepo=boundary -a && echo "=== YUM PKG INFO ===" && yum info bprobe && echo "=== YUM PKG LIST ===" && yum list bprobe'
+execute 'reload-external-yum-cache' do
+  command 'yum makecache'
   action :nothing
 end
 
@@ -15,7 +14,7 @@ remote_file '/etc/yum.repos.d/boundary.repo' do
   source 'https://yum.boundary.com/boundary_centos6_64bit.repo'
   mode '00644'
   action :create_if_missing
-  notifies :run, resources(:execute => 'clean-yum-cache'), :immediately
+  notifies :run, resources(:execute => 'reload-external-yum-cache'), :immediately
   notifies :create, resources(:ruby_block => 'reload-internal-yum-cache'), :immediately
 end
 
