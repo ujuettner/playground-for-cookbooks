f77be668bfe8459f888869bd237a290d8300fe68
Udo Juettner <udo.juettner@gmail.com>
2013-04-11 11:14:36 +0200

    Try yum-config-manager.

--- yum/recipes/default.rb
+++ yum/recipes/default.rb
@@ -1,5 +1,10 @@
 execute 'clean-yum-cache' do
   command 'rm -f /var/lib/rpm/__db* && rpm --rebuilddb && yum clean all'
+  action :nothing
+end
+
+execute 'enable-repo' do
+  command 'yum-config-manager --quiet --enable boundary'
   action :run
 end
 
@@ -7,7 +12,7 @@ remote_file '/etc/yum.repos.d/boundary.repo' do
   source 'https://yum.boundary.com/boundary_centos6_64bit.repo'
   mode '0644'
   action :create_if_missing
-  notifies :run, resources(:execute => 'clean-yum-cache'), :immediately
+  notifies :run, resources(:execute => 'enable-repo'), :immediately
 end
 
 yum_package 'bprobe' do
