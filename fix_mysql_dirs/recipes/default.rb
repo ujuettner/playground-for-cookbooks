886793f4e1edbeb0e2f8f4322c656447c978b32a
Udo Juettner <udo.juettner@gmail.com>
2014-01-07 14:41:31 +0100

    Trying a pseudo-service.

--- fix_mysql_dirs/recipes/default.rb
+++ fix_mysql_dirs/recipes/default.rb
@@ -1,3 +1,16 @@
+service 'fix_mysql_dirs' do
+  action :nothing
+end
+
+template '/etc/init.d/fix_mysql_dirs' do
+  source 'fix_mysql_dirs.erb'
+  backup false
+  owner 'root'
+  group 'root'
+  mode 0700
+  notifies :start, "service[fix_mysql_dirs]"
+end
+
 execute 'FIX: ensure MySQL data owned by MySQL user' do
   command "ls -l #{node[:mysql][:datadir]} && chown -R #{node[:mysql][:user]}:#{node[:mysql][:group]} #{node[:mysql][:datadir]}"
   action :run
