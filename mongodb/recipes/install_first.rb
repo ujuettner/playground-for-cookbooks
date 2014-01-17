b000043622958fe13240b5e84aa81dd8d96c59cc
Udo Juettner <udo.juettner@gmail.com>
2014-01-17 17:46:52 +0100

    First naive version of install recipe.

---
+++ mongodb/recipes/install_first.rb
@@ -0,0 +1,9 @@
+bash 'download the mongodb archive and extract it to /opt'
+  cwd '/tmp'
+  code <<-EOC
+    wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.4.9.tgz
+    cd /opt
+    tar -xvzf http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.4.9.tgz
+    chown -R root:root /opt/mongodb-linux-x86_64-2.4.9
+  EOC
+end
