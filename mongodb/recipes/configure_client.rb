d655438be61bcf65683d38c2d630d83fe3f26806
Udo Juettner <udo.juettner@gmail.com>
2014-01-21 00:23:48 -0800

    Get correct data structure when no replSet wanted.

--- mongodb/recipes/configure_client.rb
+++ mongodb/recipes/configure_client.rb
@@ -11,11 +11,12 @@ node['deploy'].each do |application, deploy|
 
   current_dir = ::File.join(deploy['deploy_to'], 'current')
 
+  instances = node['opsworks']['layers']['mongodb']['instances']
   mongo_config = {}
   unless node['mongodb']['replica_set'].empty?
-    mongo_config['dbUrl'] = node['opsworks']['layers']['mongodb']['instances'].map{|k,v| "#{v['private_ip']}:#{node['mongodb']['port']}/?auto_reconnect=true"}
+    mongo_config['dbUrl'] = instances.map{|k,v| "#{v['private_ip']}:#{node['mongodb']['port']}/?auto_reconnect=true"}
   else
-    mongo_config['dbUrl'] = "#{node['opsworks']['layers']['mongodb']['instances'].first['private_ip'] rescue 'localhost'}:#{node['mongodb']['port']}/?auto_reconnect=true"
+    mongo_config['dbUrl'] = "#{instances[instances.keys.first]['private_ip'] rescue 'localhost'}:#{node['mongodb']['port']}/?auto_reconnect=true"
   end
   mongo_config['rsName'] = node['mongodb']['replica_set']
 
