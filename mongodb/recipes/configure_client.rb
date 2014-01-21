f320fd0fcc0ad0ee01545847c40b1ff4d3a568a1
Udo Juettner <udo.juettner@gmail.com>
2014-01-20 20:34:35 -0800

    Prepare client for Replica Set or single node.

--- mongodb/recipes/configure_client.rb
+++ mongodb/recipes/configure_client.rb
@@ -7,7 +7,12 @@ node['deploy'].each do |application, deploy|
   current_dir = ::File.join(deploy['deploy_to'], 'current')
 
   mongo_config = {}
-  mongo_config['dbUrl'] = node['opsworks']['layers']['mongodb']['instances'].map{|k,v| "#{v['private_ip']}:27017/?auto_reconnect=true"}
+  unless node['mongodb']['replica_set'].empty?
+    mongo_config['dbUrl'] = node['opsworks']['layers']['mongodb']['instances'].map{|k,v| "#{v['private_ip']}:#{node['mongodb']['port']}/?auto_reconnect=true"}
+  else
+    mongo_config['dbUrl'] = "#{node['opsworks']['layers']['mongodb']['instances'].first['private_ip'] rescue 'localhost'}:#{node['mongodb']['port']}/?auto_reconnect=true"
+  end
+  mongo_config['rsName'] = node['mongodb']['replica_set']
 
   template ::File.join(current_dir, 'mongo.json') do
     source 'mongo.json.erb'
