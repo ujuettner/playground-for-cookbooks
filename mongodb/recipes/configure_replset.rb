3c6c9915fb5d19db30fc1337f6c915977567bd21
Udo Juettner <udo.juettner@gmail.com>
2014-02-07 23:45:49 +0100

    Add Replica Set initiate.

--- mongodb/recipes/configure_replset.rb
+++ mongodb/recipes/configure_replset.rb
@@ -21,7 +21,7 @@ end
 
 ruby_block 'initiate Replica Set' do
   block do
-    MongoReplSet.initiate(node['mongodb']['bind_ip'], node['mongodb']['port']) if is_first_mongodb_node_me
+    MongoReplSet.initiate(node['mongodb']['bind_ip'], node['mongodb']['port'], node['mongodb']['replica_set']) if is_first_mongodb_node_me
   end
 end
 
