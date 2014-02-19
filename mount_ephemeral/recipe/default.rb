d7046a7eb101d1a17f72cd9fe5a59ee3d6b424e8
Udo Juettner <udo.juettner@gmail.com>
2014-02-19 18:45:16 +0100

    First try.

---
+++ mount_ephemeral/recipe/default.rb
@@ -0,0 +1,14 @@
+node["filesystem"].each do |device, filesystem|
+  next if filesystem["mount"]
+
+  mount_point = device.gsub("/", "_")
+
+  directory mount_point do
+    action :create
+  end
+
+  mount mount_point do
+    device device
+    action [:mount, :enable]
+  end
+end
