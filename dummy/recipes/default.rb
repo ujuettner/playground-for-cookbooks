cb08a0e04d9fa50c7c5d1c99eebf9afa55f76b74
Udo Juettner <udo.juettner@gmail.com>
2014-03-16 22:01:01 +0100

    Restart service directly.

--- dummy/recipes/default.rb
+++ dummy/recipes/default.rb
@@ -1,9 +1,13 @@
 if node['opsworks']['instance']['layers'].include?('java-app')
   include_recipe "opsworks_java::#{node['opsworks_java']['java_app_server']}_service"
 
-  execute "trigger #{node['opsworks_java']['java_app_server']} service restart" do
-    command '/bin/true'
-    notifies :restart, "service[#{node['opsworks_java']['java_app_server']}]", :immediately
+  #execute "trigger #{node['opsworks_java']['java_app_server']} service restart" do
+  #  command '/bin/true'
+  #  notifies :restart, "service[#{node['opsworks_java']['java_app_server']}]", :immediately
+  #end
+
+  service node['opsworks_java']['java_app_server'] do
+    action :restart
   end
 end
 
