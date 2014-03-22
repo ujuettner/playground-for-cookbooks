3c7bb4591eabcb0559682ee59ed0a40b9211b49f
Udo Juettner <udo.juettner@gmail.com>
2014-03-22 23:58:23 +0100

    Add check for rbenv-patched commandline instead of plain executable existence.
    Delete full patch.

--- opsworks_commons/libraries/zzz_monkey_patch_rubygems_provider.rb
+++ opsworks_commons/libraries/zzz_monkey_patch_rubygems_provider.rb
@@ -6,6 +6,39 @@ class Chef
   class Provider
     class Package
       class Rubygems < Chef::Provider::Package
+        class GemEnvironment
+          ##
+          # Lists the installed versions of +gem_dep.name+, constrained by the
+          # version spec in +gem_dep.requirement+
+          # Uses OpsWorksUserSpaceGems.installed_versions to find gem in the
+          # OpsWorks user space ruby environment if @gem_binary_location id defined
+          #
+          # === Arguments
+          # Gem::Dependency   +gem_dep+ is a Gem::Dependency object, its version
+          #                   specification constrains which gems are returned.
+          # === Returns
+          # [Gem::Specification]  an array of Gem::Specification objects
+          def installed_versions(gem_dep)
+            # @gem_binary_location is defined in Chef::Resource::GemPackage
+            if @gem_binary_location.present? && correct_commandline?(@gem_binary_location)
+                OpsWorksUserSpaceGems.new(@gem_binary_location).installed_versions(
+                  gem_dep.name,
+                  gem_dep.requirement
+                )
+            elsif Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.8.0')
+                gem_specification.find_all_by_name(gem_dep.name, gem_dep.requirement)
+              else
+                gem_source_index.search(gem_dep)
+            end
+          end
+
+          private
+
+          def correct_commandline?(command_line)
+            system("#{cmd} --help > /dev/null 2>&1")
+          end
+        end
+
         #
         # manage opsworks user space gems
         class OpsWorksUserSpaceGems
