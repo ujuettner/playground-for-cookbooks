89ec68f0139dca47f9e89c8ca6367abe7eb3db3b
Udo Juettner <udo.juettner@gmail.com>
2014-06-18 23:30:41 +0200

    Fix options.

--- opsworks_tmpfs/recipes/default.rb
+++ opsworks_tmpfs/recipes/default.rb
@@ -3,11 +3,7 @@
     pass     0
     fstype   'tmpfs'
     device   '/dev/null'
-    options  "size=#{node[:opsworks_tmpfs][dir][:size]},\
-              nr_inodes=#{node[:opsworks_tmpfs][dir][:nr_inodes]},
-              mode=#{node[:opsworks_tmpfs][dir][:mode]},\
-              uid=#{node[:opsworks_tmpfs][dir][:uid]},\
-              gid=#{node[:opsworks_tmpfs][dir][:gid]}"
+    options  "size=#{node[:opsworks_tmpfs][dir][:size]},nr_inodes=#{node[:opsworks_tmpfs][dir][:nr_inodes]},mode=#{node[:opsworks_tmpfs][dir][:mode]},uid=#{node[:opsworks_tmpfs][dir][:uid]},gid=#{node[:opsworks_tmpfs][dir][:gid]}"
     action   [:mount, :enable]
   end
 end
