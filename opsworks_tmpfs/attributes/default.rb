5b88b8f9ddc8cd36f05aec4e101c272830cb2f82
Udo Juettner <udo.juettner@gmail.com>
2014-06-20 16:20:37 +0200

    Add device, change UID/GID and split options into several lines.

--- opsworks_tmpfs/attributes/default.rb
+++ opsworks_tmpfs/attributes/default.rb
@@ -1,20 +1,23 @@
 default[:opsworks_tmpfs][:cookbooks_cache][:path] = ::File.join(Opsworks::InstanceAgent::Environment.file_cache_path, 'cookbooks')
+default[:opsworks_tmpfs][:cookbooks_cache][:device] = 'tmpfs'
 default[:opsworks_tmpfs][:cookbooks_cache][:size] = '200m'
 default[:opsworks_tmpfs][:cookbooks_cache][:nr_inodes] = '999k'
 default[:opsworks_tmpfs][:cookbooks_cache][:mode] = '755'
-default[:opsworks_tmpfs][:cookbooks_cache][:uid] = 0
-default[:opsworks_tmpfs][:cookbooks_cache][:gid] = 0
+default[:opsworks_tmpfs][:cookbooks_cache][:uid] = 'aws'
+default[:opsworks_tmpfs][:cookbooks_cache][:gid] = 'aws'
 
 default[:opsworks_tmpfs][:berkshelf_cache][:path] = Opsworks::InstanceAgent::Environment.berkshelf_cache_path
+default[:opsworks_tmpfs][:berkshelf_cache][:device] = 'tmpfs'
 default[:opsworks_tmpfs][:berkshelf_cache][:size] = '200m'
 default[:opsworks_tmpfs][:berkshelf_cache][:nr_inodes] = '999k'
 default[:opsworks_tmpfs][:berkshelf_cache][:mode] = '755'
-default[:opsworks_tmpfs][:berkshelf_cache][:uid] = 0
-default[:opsworks_tmpfs][:berkshelf_cache][:gid] = 0
+default[:opsworks_tmpfs][:berkshelf_cache][:uid] = 'aws'
+default[:opsworks_tmpfs][:berkshelf_cache][:gid] = 'aws'
 
 default[:opsworks_tmpfs][:data][:path] = '/var/lib/aws/opsworks/data'
+default[:opsworks_tmpfs][:data][:device] = 'tmpfs'
 default[:opsworks_tmpfs][:data][:size] = '500m'
 default[:opsworks_tmpfs][:data][:nr_inodes] = '999k'
-default[:opsworks_tmpfs][:data][:mode] = '755'
-default[:opsworks_tmpfs][:data][:uid] = 0
-default[:opsworks_tmpfs][:data][:gid] = 0
+default[:opsworks_tmpfs][:data][:mode] = '700'
+default[:opsworks_tmpfs][:data][:uid] = 'aws'
+default[:opsworks_tmpfs][:data][:gid] = 'aws'
--- opsworks_tmpfs/recipes/default.rb
+++ opsworks_tmpfs/recipes/default.rb
@@ -2,8 +2,14 @@
   mount node[:opsworks_tmpfs][dir][:path] do
     pass     0
     fstype   'tmpfs'
-    device   '/dev/null'
-    options  "size=#{node[:opsworks_tmpfs][dir][:size]},nr_inodes=#{node[:opsworks_tmpfs][dir][:nr_inodes]},mode=#{node[:opsworks_tmpfs][dir][:mode]},uid=#{node[:opsworks_tmpfs][dir][:uid]},gid=#{node[:opsworks_tmpfs][dir][:gid]}"
+    device   node[:opsworks_tmpfs][dir][:device]
+    options  [
+      "size=#{node[:opsworks_tmpfs][dir][:size]}",
+      "nr_inodes=#{node[:opsworks_tmpfs][dir][:nr_inodes]}",
+      "mode=#{node[:opsworks_tmpfs][dir][:mode]}",
+      "uid=#{node[:opsworks_tmpfs][dir][:uid]}",
+      "gid=#{node[:opsworks_tmpfs][dir][:gid]}"
+    ].join
     action   [:mount, :enable]
   end
 end
