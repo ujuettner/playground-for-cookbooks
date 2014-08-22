cec20333a8504fb7d3afae48738f5c8ad9a1ca32
Udo Juettner <udo.juettner@gmail.com>
2014-08-22 11:55:07 +0200

    Fix env and add output redirection.

--- cron_test/recipes/default.rb
+++ cron_test/recipes/default.rb
@@ -1,12 +1,12 @@
 cron "first task" do
   path "/usr/bin:/usr/local/bin"
   minute "*/2"
-  command "cd /srv/www/myapp/current && bundle exec ruby -v"
+  command "cd /srv/www/myapp/current && bundle exec ruby -v > /tmp/first_task 2>&1"
 end
 
-cron_env = {PATH => "/usr/local/bin:$PATH"}
+cron_env = {"PATH" => "/usr/local/bin:$PATH"}
 cron "second task" do
   environment cron_env
   minute "*/2"
-  command "cd /srv/www/myapp/current && bundle exec ruby -v"
+  command "cd /srv/www/myapp/current && bundle exec ruby -v > /tmp/second_task 2>&1"
 end
