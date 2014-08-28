2cad5a1427943424f4fa431437e86826973450cd
Udo Juettner <udo.juettner@gmail.com>
2014-08-28 18:17:41 +0200

    Patch BlockDevice::assemble_raid.

--- ebs/libraries/block_device_patch.rb
+++ ebs/libraries/block_device_patch.rb
@@ -1,32 +1,37 @@
 module BlockDevice
-  def self.create_lvm(raid_device, options)
-    Chef::Log.info "creating LVM volume out of #{raid_device} with #{options[:disks].size} disks at #{options[:mount_point]}"
-    unless lvm_physical_group_exists?(raid_device)
-      exec_command("pvcreate #{raid_device}") or raise "Failed to create LVM physical disk for #{raid_device}"
-    end
-    unless lvm_volume_group_exists?(raid_device)
-      exec_command("vgcreate #{lvm_volume_group(raid_device)} #{raid_device}") or raise "Failed to create LVM volume group for #{raid_device}"
-    end
-    unless lvm_volume_exits?(raid_device)
-      extends = `vgdisplay #{lvm_volume_group(raid_device)} | grep Free`.scan(/\d+/)[0]
-      unless exec_command_with_retries("lvcreate -l #{extends} #{lvm_volume_group(raid_device)} -n #{File.basename(lvm_device(raid_device))}")
-        exec_command("lvcreate -l #{extends} #{lvm_volume_group(raid_device)} -n #{File.basename(lvm_device(raid_device))} -Z n") or raise "Failed to create the LVM volume at #{raid_device}"
+  def self.assemble_raid(raid_device, options)
+    Chef::Log.info("Resuming existing RAID array #{raid_device} with #{options[:disks].size} disks, RAID level #{options[:raid_level]} at #{options[:mount_point]}")
+    unless exec_command("mdadm --assemble --verbose #{raid_device} #{options[:disks].join(' ')}")
+      plain_disks = options[:disks].map{|disk| disk.gsub('/dev/', '')}
+      affected_volume_groups = []
+      File.readlines('/proc/mdstat').each do |line|
+        md_device = nil
+        md_device = line.split.first if plain_disks.any?{|disk| line.include?(disk)}
+        if md_device
+          begin
+            physical_volume_info = OpsWorks::ShellOut.shellout("pvdisplay -c /dev/#{md_device}").lines.select{|pv_line| pv_line =~ /\/dev\/#{md_device}/}.first
+            if physical_volume_info
+              volume_group = physical_volume_info.split(':')[1] rescue nil
+              if volume_group
+                affected_volume_groups << volume_group
+                Chef::Log.info("Deactivating volume group #{volume_group}")
+                exec_command("vgchange --available n #{volume_group}")
+              end
+            end
+          rescue RuntimeError => e
+            Chef::Log.debug("Getting attributes of /dev/#{md_device} failed: #{e.class} - #{e.message} - #{e.backtrace.join("\n")}")
+            Chef::Log.info("Getting attributes of /dev/#{md_device} failed: #{e.message}")
+          ensure
+            Chef::Log.info("Stopping /dev/#{md_device}")
+            exec_command("mdadm --stop --verbose /dev/#{md_device}")
+          end
+        end
+      end
+      exec_command("mdadm --assemble --verbose #{raid_device} #{options[:disks].join(' ')}") or raise "Failed to assemble the RAID array at #{raid_device}"
+      affected_volume_groups.each do |volume_group|
+        Chef::Log.info "(Re-)activating volume group #{volume_group}"
+        exec_command("vgchange --available y #{volume_group}")
       end
     end
-  end
-
-  def self.exec_command_with_retries(command, max_tries=3, exponential_sleep_time_factor=10)
-    try_count = 1
-    while try_count <= max_tries
-      Chef::Log.info "Try #{try_count}/#{max_tries}: #{command}"
-      break if exec_command(command)
-      sleep_time = exponential_sleep_time_factor * (2 ** (try_count - 1))
-      Chef::Log.info "Try #{try_count}/#{max_tries} for '#{command}' failed - retrying in #{sleep_time} seconds."
-      sleep sleep_time
-      try_count += 1
-    end
-    success = try_count <= max_tries
-    Chef::Log.info "'#{command}' successful after #{try_count}/#{max_tries} tries? #{success}"
-    success
   end
 end
