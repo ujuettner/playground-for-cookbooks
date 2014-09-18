338fc2a38267bc4c1ff2ff781abbd52dd3386e69
Udo Juettner <udo.juettner@gmail.com>
2014-09-18 10:59:58 +0200

    Add another cookbook.

---
+++ bear/metadata.rb
@@ -0,0 +1,7 @@
+name        "bear"
+description "Yet another cookbook"
+license     "Apache 2.0"
+version     "0.0.1"
+
+depends "apache2"
+
---
+++ bear/recipes/setup.rb
@@ -0,0 +1,27 @@
+node[:deploy].each do |_, deploy|
+  app_root = ::File.join(deploy[:deploy_to], "current")
+
+  template "#{::File.join(app_root, "db-connect.php")}" do
+    source "db-connect.php.erb"
+    mode 0775
+    owner node[:apache][:user]
+    group deploy[:group]
+
+    variables(
+      :host => (deploy[:database][:host] rescue nil),
+      :user => (deploy[:database][:username] rescue nil),
+      :password => (deploy[:database][:password] rescue nil),
+      :db => (deploy[:database][:database] rescue nil),
+      :domain => (deploy[:deploy][:domains].first rescue "http://sandbox.example.net/" ),
+      :rootpath => ::File.realpath(app_root)
+    )
+
+    only_if do
+      File.directory?(app_root)
+    end
+  end
+
+  execute "chmod -R 775 #{app_root}" do
+  end
+end
+
